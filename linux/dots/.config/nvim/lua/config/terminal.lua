72645357e4e27e6e9577395d7999d6c985a58f69
haseeb <haseebtariq94@hotmail.com>
2024-09-29 14:47:02 +0500

    Updated NeoVim temrinal config

--- linux/dots/.config/nvim/lua/config/terminal.lua
+++ linux/dots/.config/nvim/lua/config/terminal.lua
@@ -1,5 +1,7 @@
 vim.api.nvim_command('autocmd TermOpen * startinsert')
-vim.api.nvim_command('autocmd TermOpen * setlocal nonumber')
+vim.api.nvim_command('autocmd TermOpen * setlocal nonu')
+vim.api.nvim_command('autocmd TermOpen * setlocal nornu')
+vim.api.nvim_command('autocmd TermOpen * setlocal nocursorline')
 vim.api.nvim_command('autocmd TermOpen * setlocal signcolumn=no')
 
 vim.keymap.set('t', '<esc>', '<C-\\><C-n>')
